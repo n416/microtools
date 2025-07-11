eda1c953470e695473f6d4f3d9d5e1ba5867bc35
n416 <nakamura.shingo@gmail.com>
2025-07-12 01:44:40 +0900

    時計を追加

--- nekokan/main.js
+++ nekokan/main.js
@@ -1,30 +1,35 @@
-import { showOverwriteModal } from './ui.js';
-import { loadFromUrlParams, loadLogs, removeHistoryGetData } from './storage.js';
-import { initializeEventListeners } from './events.js';
-import { updateNoteCard, updateAreaCount} from './ui.js';
+import {showOverwriteModal} from './ui.js';
+import {loadFromUrlParams, loadLogs, removeHistoryGetData} from './storage.js';
+import {initializeEventListeners} from './events.js';
+import {updateNoteCard, updateAreaCount, updateDigitalClock} from './ui.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const existingData = loadLogs();  // ローカルストレージ内のデータを確認
+  const existingData = loadLogs(); // ローカルストレージ内のデータを確認
 
-  if (new URLSearchParams(window.location.search).has('data') && existingData.length > 0) {
-
+  if (
+    new URLSearchParams(window.location.search).has('data') &&
+    existingData.length > 0
+  ) {
     // 上書き確認モーダルを表示して、ユーザーの選択後に処理を続ける
-    showOverwriteModal(() => {
-      // ユーザーがOKを押した場合、GETパラメータのデータをロードしてから初期化
-      loadFromUrlParams();
-      initializeEventListeners();
-      updateNoteCard(); // ノートカードを更新
-      updateAreaCount();
-      // URLからGETパラメータを削除
-      removeHistoryGetData();
-    }, () => {
-      // ユーザーがキャンセルを押した場合、通常の初期化
-      initializeEventListeners();
-      updateNoteCard();
-      updateAreaCount();
-      // URLからGETパラメータを削除
-      removeHistoryGetData();
-    });
+    showOverwriteModal(
+      () => {
+        // ユーザーがOKを押した場合、GETパラメータのデータをロードしてから初期化
+        loadFromUrlParams();
+        initializeEventListeners();
+        updateNoteCard(); // ノートカードを更新
+        updateAreaCount();
+        // URLからGETパラメータを削除
+        removeHistoryGetData();
+      },
+      () => {
+        // ユーザーがキャンセルを押した場合、通常の初期化
+        initializeEventListeners();
+        updateNoteCard();
+        updateAreaCount();
+        // URLからGETパラメータを削除
+        removeHistoryGetData();
+      }
+    );
   } else {
     // 上書き確認の必要がない場合は直接データをロード
     loadFromUrlParams();
@@ -35,4 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   // URLからGETパラメータを削除
   setInterval(updateNoteCard, 5000);
+  // デジタル時計の初期化と更新（1秒ごと）
+  updateDigitalClock(); // ページ読み込み時に即時表示
+  setInterval(updateDigitalClock, 1000);
 });
