6da69fd4f629913cb87c6ad96823804235d403c8
n416 <parunaporute@gmail.com>
2026-03-30 09:45:39 +0900

    ストーリー追加

--- yomimono2/scripts/generate_sidebar.js
+++ yomimono2/scripts/generate_sidebar.js
@@ -1,17 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { episodeSequence } from '../episode_sequence.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const episodeSequence = [
-  'ep0000', 'ep0100', 'ep0200', 'ep0300', 'ep0400', 'ep0420', 'ep0450',
-  'ep0500', 'ep0600', 'ep0650', 'ep0660', 'ep0700', 'ep0800', 'ep0900'
-];
-
 const settingsPath = path.join(__dirname, '..', 'public', 'settings');
 const outputPath = path.join(__dirname, '..', 'src', 'sidebar_nav.js');
+
 
 const navItems = [];
 
