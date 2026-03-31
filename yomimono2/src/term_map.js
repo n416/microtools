5f2c433d3697d00d8532738461f490eca5341fae
n416 <parunaporute@gmail.com>
2026-03-31 22:09:26 +0900

    泥削除

--- yomimono2/src/term_map.js
+++ yomimono2/src/term_map.js
@@ -1,12 +1,11 @@
 // 自動生成ファイル: npm run dev または npm run build 時に自動で更新されます
 // ここを手動で編集しても上書きされるため注意してください
 export const TermFirstAppearanceMap = {
-  "life_value_efficiency": "ep0000",
-  "water_lily": "ep0000",
-  "life_value": "ep0005",
+  "life_value_efficiency": "ep0005",
+  "life_value": "ep0020",
   "advanced_student": "ep0030",
-  "conduit_performance": "ep0090",
   "space_time_magic": "ep0106",
+  "water_lily": "ep0118",
   "master_key": "ep0118",
   "magic_reduction_furnace": "ep0180"
 };
