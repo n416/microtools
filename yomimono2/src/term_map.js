32243441c196bb6fd289cd03059b1addf61414b1
n416 <nakamura.shingo@gmail.com>
2026-04-06 09:15:55 +0900

    再構築

--- yomimono2/src/term_map.js
+++ yomimono2/src/term_map.js
@@ -1,12 +1,10 @@
 // 自動生成ファイル: npm run dev または npm run build 時に自動で更新されます
 // ここを手動で編集しても上書きされるため注意してください
 export const TermFirstAppearanceMap = {
-  "life_value_efficiency": "ep0010",
-  "space_time_magic": "ep0010",
-  "life_value": "ep0030",
-  "advanced_student": "ep0040",
+  "advanced_student": "ep0070",
   "sensei": "ep0170",
+  "life_value": "ep0190",
   "water_lily": "ep0300",
   "master_key": "ep0300",
-  "magic_reduction_furnace": "ep0340"
+  "magic_reduction_furnace": "ep0390"
 };
