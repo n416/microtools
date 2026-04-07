5b78530f3623c581200daaad906055ab21c1e0a2
n416 <parunaporute@gmail.com>
2026-04-07 20:52:51 +0900

    推敲

--- yomimono2/src/term_map.js
+++ yomimono2/src/term_map.js
@@ -1,6 +1,7 @@
 // 自動生成ファイル: npm run dev または npm run build 時に自動で更新されます
 // ここを手動で編集しても上書きされるため注意してください
 export const TermFirstAppearanceMap = {
+  "life_value_efficiency": "ep0040",
   "advanced_student": "ep0070",
   "sensei": "ep0170",
   "life_value": "ep0190",
