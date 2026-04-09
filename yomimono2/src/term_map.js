40a56f83beaf75c3e834cad65a1eaed288927f37
n416 <parunaporute@gmail.com>
2026-04-09 15:49:03 +0900

    推敲

--- yomimono2/src/term_map.js
+++ yomimono2/src/term_map.js
@@ -2,9 +2,9 @@
 // ここを手動で編集しても上書きされるため注意してください
 export const TermFirstAppearanceMap = {
   "life_value_efficiency": "ep0040",
+  "magic_reduction_furnace": "ep0060",
   "advanced_student": "ep0070",
   "sensei": "ep0170",
   "life_value": "ep0190",
-  "master_key": "ep0265",
-  "magic_reduction_furnace": "ep0320"
+  "master_key": "ep0265"
 };
